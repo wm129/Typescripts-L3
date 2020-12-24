0f6897d124a69d0da8cb063588e5a62678188334
wm129 <sekaimanyuucamp@gmail.com>
2020-12-24 23:20:52 +0900

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express()
+//エンドポイント　”/”にGETリクエストされた時に返す内容を指している
+app.get('/', (req, res) => {
+    res.send('hello, world');
+});
+app.get('/user', (req, res) => {
+    res.status(200).json({ username: 'Mayu', age: 24 });
+});
+app.listen(8000, () => {
+    console.log('server listening on port 8000...');
+});
